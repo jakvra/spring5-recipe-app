e7ae2982b708cb0c0927e7c0910330b9d252871d
Jakub Vrana <jvrana@monetplus.cz>
2019-01-23 14:56:13 +0100

    Added sufire configuration

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -16,7 +16,7 @@ public class IndexController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping({"", "/", "index"})
+    @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
         log.debug("Getting Index page");
 
